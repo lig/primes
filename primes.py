97e1987e4ab0759d1c0a73617b599dd0c288483c
Serge Matveenko <s@matveenko.ru>
2012-03-05 01:46:33 +0400

    Use only primes for "j".

--- primes.py
+++ primes.py
@@ -1,14 +1,13 @@
-from collections import deque
 from datetime import datetime
 
 
 start = datetime.now()
 
-primes = deque([0, 1])
+primes = [0, 1]
 
 for i in range(2, 10000):
 
-    for j in range(2, (i - 1)):
+    for j in primes[2:]:
 
         if i % j == 0:
             i = None
