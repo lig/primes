b163fd7e1b8a2d6a7705e532b346c41563eb3a55
Serge Matveenko <s@matveenko.ru>
2012-03-05 01:42:33 +0400

    Use "i" as flag.

--- primes.py
+++ primes.py
@@ -7,15 +7,14 @@ start = datetime.now()
 primes = deque([0, 1])
 
 for i in range(2, 10000):
-    is_prime = True
 
     for j in range(2, (i - 1)):
 
         if i % j == 0:
-            is_prime = False
+            i = None
             break
 
-    if is_prime:
+    if i:
         primes.append(i)
 
 stop = datetime.now()
