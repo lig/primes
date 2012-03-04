48e25921a09c1f3b613a058438f45787df85c102
Serge Matveenko <s@matveenko.ru>
2012-03-05 01:19:01 +0400

    Use datetime and timedelta instead of time.

--- primes.py
+++ primes.py
@@ -1,12 +1,14 @@
-import time
+from datetime import datetime
 
 primes = range(0, 10000)
 
-t = time.time()
+start = datetime.now()
 
 for i in range(2, 10000):
     for j in range(2, (i - 1)):
         if (primes[i] != 0) and (primes[i] % j == 0):
             primes[i] = 0
 
-print((time.time() - t))
+stop = datetime.now()
+
+print(stop - start)
