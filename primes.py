b9f99c8ce8bea62701f45768f9240e02eb8f46ae
Serge Matveenko <s@matveenko.ru>
2012-03-05 01:17:12 +0400

    Fix Python code style.

--- primes.py
+++ primes.py
@@ -1,12 +1,12 @@
 import time
 
-primes = range(0,10000)
+primes = range(0, 10000)
 
 t = time.time()
 
 for i in range(2, 10000):
-    for j in range(2, (i-1)):
+    for j in range(2, (i - 1)):
         if (primes[i] != 0) and (primes[i] % j == 0):
-            primes[i] = 0;
+            primes[i] = 0
 
 print((time.time() - t))
