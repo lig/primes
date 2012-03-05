acf1cab0a18127684307f101340e78b7ef62b9ab
konstantin-rem <konstantin.rem@gmail.com>
2012-03-05 14:00:28 +0400

    Update

--- primes.rb
+++ primes.rb
@@ -3,16 +3,19 @@ primes = Array.new
 t = Time.now.to_f
 
 for i in (2..10000)
-  is_prime = true
+  k = Math.sqrt(i).to_i()
 
   for j in (2..(i-1))
+
+    break if j > k
+
     if i % j == 0 then
-      is_prime = false
+      i = nil
       break
     end
   end
 
-  if is_prime == true then
+  if i then
     primes.push(i)
   end
 end
